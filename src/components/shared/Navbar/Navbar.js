11898cbd775f5eade2c067963a94853ba730272c
jahidshawon19 <jahidshawon1730@gmail.com>
2023-06-13 10:45:14 +0600

    navbar completed

---
+++ src/components/shared/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { HiSearch } from "react-icons/hi";
+import './Navbar.css'; 
+
+const Navbar = () => {
+    return (
+                <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">Learning Edge</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <div className='menuSearch'>
+                        <input className='inputSearch' type='text'  placeholder='What do you want to learn?'/>
+                    </div>
+                    <button className='seacrhBtn   btn'><HiSearch></HiSearch></button>
+             
+
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto">
+                  
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary" href="#">Log in</a>
+                    </li>
+
+                   
+                    <li class="nav-item">
+                        <button className='signUPBtn btn'><b>Join for Free</b></button>
+                    </li>
+                  
+
+
+                    </ul>
+
+                </div>
+                </nav>
+    );
+};
+
+export default Navbar;
